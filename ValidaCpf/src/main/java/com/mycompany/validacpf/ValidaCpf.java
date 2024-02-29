0a128a04c6d3b9f6f63219c45f97f471dacd2699
Ricardo Santos <ricardoldsantos@hotmail.com>
2024-02-29 20:06:46 -0300

    ValidaCpf

---
+++ ValidaCpf/src/main/java/com/mycompany/validacpf/ValidaCpf.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.validacpf;
+
+/**
+ *
+ * @author noteb
+ */
+public class ValidaCpf {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
